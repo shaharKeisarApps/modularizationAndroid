a29b3101f8bb7a43c4d4b4885925841d963cecd6
GitHub Action <action@github.com>
2023-02-04 13:20:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/shaharkeisarapps/modularizationandroid/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/shaharkeisarapps/modularizationandroid/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.shaharkeisarapps.modularizationandroid.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.shaharkeisarapps.modularizationandroid.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/shaharkeisarapps/modularizationandroid/services/MyProjectService.kt
+++ src/main/kotlin/com/github/shaharkeisarapps/modularizationandroid/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.shaharkeisarapps.modularizationandroid.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.shaharkeisarapps.modularizationandroid.MyBundle
 
 class MyProjectService(project: Project) {
 
